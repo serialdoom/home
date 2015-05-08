5c00bbb926ad30ab50b35714ee960afd0ddb8b29
Mike <serialdoom@gmail.com>
2015-05-08 14:41:29 +0100

    bin/fortune.sh - initial import

---
+++ bin/fortune.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+FILE=/home/mc42/.fortunes
+
+
+if [[ ! -f $FILE ]]; then
+    echo "Error, fortune file [$FILE] not found";
+    exit 1;
+fi
+
+
+CNT=$[ $(grep "^%" -c $FILE) / 2 ];
+RAND=$(perl -e "print(int(rand($CNT)))");
+#echo "CNT is $CNT, rand is $RAND";
+
+perl -n -e "BEGIN {\$cnt = $RAND}; \$cnt -= 1 if (\$_ =~ /^%/); print \$_ if (\$cnt == 0 && \$_ !~ /^%/);" $FILE
+
