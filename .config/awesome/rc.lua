d2467a88bfe908bc6f266868d7f851c9ca562557
Mike Christofilopoulos <mchristof@decibelinsight.com>
2015-08-03 09:27:36 +0100

    awesome: fixed chrome class

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -340,7 +340,7 @@ awful.rules.rules = {
     { rule = { class = "gimp" },
       properties = { floating = true } },
     -- Set Firefox to always map on tags number 2 of screen 1.
-    { rule = { class = "Chromium" },
+    { rule = { class = "Chromium-browser" },
         properties = { tag = tags[1][2] } },
     { rule = { class = "Firefox" },
         properties = { tag = tags[1][2] } },
