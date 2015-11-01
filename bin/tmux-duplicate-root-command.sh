d6554b49db798b38c96aa71952af333bfff6d5a9
Mike <mchristof@decibelinsight.com>
2015-11-01 11:07:32 +0000

    tmux-duplicate: if no command is available, bash

--- bin/tmux-duplicate-root-command.sh
+++ bin/tmux-duplicate-root-command.sh
@@ -9,4 +9,8 @@ ROOT_DEV_PTS=$(echo $CURRENT_SESSION | grep -oP "/dev/pts/\d*")
 ROOT_PTS=$(echo $ROOT_DEV_PTS | perl -p -e 's!/dev/!!g')
 #echo $ROOT_PTS
 ROOT_CMD=$(ps -ao pid,tty,args | grep $ROOT_PTS | grep -v grep | tr -s ' ' | cut -d ' ' -f3-)
+echo "eval [$ROOT_CMD]" >> /tmp/tmux.log
 eval $ROOT_CMD
+echo "$?" >> /tmp/tmux.log
+[ $? -ne 0 ] && /usr/bin/env bash
+[ "$ROOT_CMD" == "" ] && /usr/bin/env bash
